b0fd3857257a9536f139ed155686989c8871997a
tregelacio <tre.gelacio@gmail.com>
2021-10-25 01:43:04 -1000

    Outlined backend to work with the frontend

---
+++ config/production.js
@@ -0,0 +1,13 @@
+module.exports = {
+    bucket: {
+      slug: process.env.COSMIC_BUCKET,
+      read_key: process.env.COSMIC_READ_KEY,
+      write_key: process.env.COSMIC_WRITE_KEY
+    },
+    twilio: {
+      auth: process.env.TWILIO_AUTH,
+      sid: process.env.TWILIO_SID,
+      number: process.env.TWILIO_NUMBER
+    }
+  }
+  
